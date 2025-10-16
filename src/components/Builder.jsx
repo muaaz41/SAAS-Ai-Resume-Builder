9f003e77275379aefbaafea508badbac816f0dd8
muaaz41 <munirmuaaz@gmail.com>
2025-10-16 22:21:55 +0500

    build issue

--- src/components/Builder.jsx
+++ src/components/Builder.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "../context/AuthContext.jsx";
 import { useLocation } from "react-router-dom";
 import RichTextEditor from "./RichTextEditor.jsx";
 import Loader from "./Loader.jsx";
-import Loader from "./Loader.jsx";
+// import Loader from "./Loader.jsx";
 
 // ------------------------------
 // Helper: sanitize resume payload
